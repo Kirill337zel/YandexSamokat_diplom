8fc783271eba5ed92509b32a4795550e18ba7915
Kirill337zel <galagan1972@gmail.com>
2025-03-06 21:26:18 +0300

    io

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://b13177eb-6fd6-4a9f-937d-536e8a246134.serverhub.praktikum-services.ru"
+URL_SERVICE = "https://51063642-7ac2-465e-b0da-08b53a096599.serverhub.praktikum-services.ru"
 CREATE_ORDERS = "/api/v1/orders"
 ORDER_INFORMATION = "/api/v1/orders/track?t="
