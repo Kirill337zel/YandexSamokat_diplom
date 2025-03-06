dfab56147227401977d53f8f0597d904e573621f
Kirill337zel <galagan1972@gmail.com>
2025-03-06 18:48:41 +0300

    da

---
+++ data.py
@@ -0,0 +1,13 @@
+order_body = {
+    "firstName": "Uchiha",
+    "lastName": "Naruto",
+    "address": "Konoha, 143 apt.",
+    "metroStation": 3,
+    "phone": "+7 800 355 30 30",
+    "rentTime": 3,
+    "deliveryDate": "2024-03-10",
+    "comment": "Saske, come back to Konoha",
+    "color": [
+        "BLACK"
+    ]
+}
