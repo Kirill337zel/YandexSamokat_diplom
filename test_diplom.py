e9c8d37d12ebb0ed3c3e0d73aa4788a93467aa7b
Kirill337zel <galagan1972@gmail.com>
2025-03-06 20:49:21 +0300

    eu

---
+++ test_diplom.py
@@ -0,0 +1,43 @@
+# Кирилл Гологанов, 27-я когорта — Дипломный проект. Инженер по тестированию плюс
+
+import configuration
+import data
+import requests
+
+# Создание заказа
+def create_order(body):
+    return requests.post (configuration.URL_SERVICE + configuration.CREATE_ORDERS,
+                         json=body)
+
+def create_order(order_body):
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_ORDERS,
+                         json=order_body)
+
+# Получение заказа по номеру трекера
+def get_order(track_number):
+    get_order_url = f"{configuration.URL_SERVICE}/api/v1/orders/track?t={track_number}"
+    response = requests.get(get_order_url)
+    return response
+
+def get_order_info_by_track(track_number):
+    return requests.get(configuration.URL_SERVICE + configuration.ORDER_INFORMATION + str(track_number))
+
+# Автотест
+def test_order_creation_and_retrieval():
+    response = create_order(data.order_body)
+
+    track_number = response.json()["track"]
+    print("Заказ создан. Номер трека:", track_number)
+
+# Получение данных заказа по треку
+    order_response = get_order(track_number)
+
+    assert order_response.status_code == 200, f"Ошибка: {order_response.status_code}"
+    order_data = order_response.json()
+    print("Данные заказа:")
+    print(order_data)
+
+def test_get_order_info_by_track():
+    track = request.create_order(data.order_body).json()['track']
+    response = request.get_order_info_by_track(track)
+    assert response.status_code == 200
